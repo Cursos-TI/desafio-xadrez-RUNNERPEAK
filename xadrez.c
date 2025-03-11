09e555369f0d9f91ec4c2c76cf20ca65ce8daf65
RUNNERPEAK <theviperez@gmail.com>
2025-03-11 20:09:23 +0000

    11/03

--- xadrez.c
+++ xadrez.c
@@ -1,101 +1,83 @@
 #include <stdio.h>
 
+void mover_torre(int movtorre) {
+    if (movtorre > 0 ){
+        printf("A torre foi para a direita.\n");
+        mover_torre(movtorre - 1);
+    }
+}
+
+
+
+void mover_bispo(int movbispo, int escolhabispo) {
+   if (movbispo > 0){
+    if (escolhabispo == 1){
+        printf("O bispo se moveu para cima e para direita (diagonal).\n");
+    }else if (escolhabispo == 2){
+        printf("O bispo se moveu para cima e para esquerda (diagonal).\n");
+    }
+    mover_bispo(movbispo - 1, escolhabispo);
+}
+}
+
+
+
+void mover_rainha(int movrainha){
+    if(movrainha > 0){
+        printf("A rainha se moveu para a esquerda.\n"); 
+        mover_rainha(movrainha - 1);
+    }
+}
+
+
+
+void mover_cavalo(int movcavalo){
+
+    for(int cavalo = 0; cavalo < 2; cavalo++){
+        printf("A cavalo se moveu para: cima.\n"); 
+    }
+    if (movcavalo == 1){
+        printf("O cavalo se moveu para: esquerda.\n");
+    }else if (movcavalo == 2){
+        printf("O cavalo se moveu para: direita.\n");
+    }    
+    }
+
+
+
 int main() {
-    
-    //VARIÁVEIS PARA INICIAR O CÓDIGO, REPRESENTANDO AS PEÇAS E QUANTAS CASAS VÃO SE MOVER
-    int torre, bispo, rainha, cavalo;
+
+    //VARIÁVEIS REPRESENTANDO OS MOVIMENTOS DAS PEÇAS DE XADREZ
     int movtorre, movbispo, movrainha, movcavalo;
 
-    //PEDE AO USUÁRIO DIGITAR QUANTAS CASA ELE DESEJA QUE A TORRE SE MOVE ( LENDO A ENTRANDO, E IMPRIMINDO O RESULTADO )
+    //VARIAVEL QUE PERMITIRÁ O USUÁRIO ESCOLHER SE O CAVALO IRÁ PARA A ESQUERDA OU DIREITA
+    int escolha, escolhabispo;
+
+
     printf("Digite quantas casas você deseja mover a torre para a direita: \n");
     scanf("%d", &movtorre);
-
-    //A VARIÁVEL "torre" ESTÁ SETADA EM 0, LOGO SERÁ SOMADO COM O VALOR QUE O USUÁRIO DER ENTRADA REPRESENTADO PELO "movtorre"
-    for(torre = 0; torre < movtorre; torre++){
-    printf("A torre foi para a direita.\n");
-    }
+    mover_torre(movtorre);
 
 
 
-
-    //PEDE AO USUÁRIO QUANTAS CASA ELE DESEJA QUE O BISPO SE MOVA ( LÊ, E IMPRIME O RESULTADO)
-    printf("\nDigite quantas casas você deseja mover o bispo: \n");
+    printf("\nQuantas casas você deseja mover o bispo?\n");
     scanf("%d", &movbispo);
-
-     //A VARIÁVEL "bispo" ESTÁ SETADA EM 0, LOGO SERÁ SOMADO COM O VALOR QUE O USUÁRIO DER ENTRADA REPRESENTADO PELO "movbispo"
-    bispo = 0;
-    while(bispo < movbispo){
-        printf("O bispo se moveu para cima e para direita (diagonal).\n");    
-        bispo++;
-    }
+    printf("E para diagonal direita(1) ou esquerda(2)?\n");
+    scanf("%d", &escolhabispo);
+    mover_bispo(movbispo, escolhabispo);
 
 
 
-    //PEDE AO USUARIO QUANTAS VEZES A RAINHA IRÁ SE MOVAR PARA A ESQUERDA.
     printf("\nDigite quantas casas você deseja que a rainha se mova para a esquerda: \n");
     scanf("%d", &movrainha);
-
-    //A VARIÁVEL "rainha" ESTÁ SETADA EM 0, LOGO SERÁ SOMADO COM O VALOR QUE O USUÁRIO DER ENTRADA REPRESENTADO PELO "movrainha"
-    rainha = 0;
-    do
-    {
-        printf("A rainha se moveu para a esquerda.\n"); 
-        rainha++;  
-    } while (rainha < movrainha);
+    mover_rainha(movrainha);
 
 
 
-    //VARIAVEL PARA O USUÁRIO ESCOLHER SE O CAVALO IRÁ PARA A ESQUERDA OU DIREITA
-    int escolha;
-    movcavalo = 0;
-
-    printf("\nO cavalo se move em L, você quer que ele vá para esquerda(1) ou direita(2) ?\n");
+    printf("\nO cavalo se move em L, você quer que ele vá para esquerda(1) ou direita(2)?\n");
     scanf("%d", &escolha);
-
-
-    //"movcavalo" SETADA EM 0, ENTÃO ENQUANTO FOR 0, SERÁ EXECUTADO O CÓDIGO IMPRIMINDO QUE O CAVALO FOI PARA CIMA DUAS VEZES.
-    while (movcavalo == 0){
-    
-        for(cavalo = 0; cavalo < 2; cavalo++){
-        printf("A cavalo se moveu para: cima.\n");
-    }
-
-    //SE O USUÁRIO ESCOLHER 1, IRÁ IMPRIMIR QUE ELE FOI PARA A ESQUERDA.
-    while(escolha == 1){
-        printf("O cavalo se moveu para: esquerda.\n");
-        break;
-    }
-
-    //SE O USUÁRIO ESCOLHER 2, IRÁ IMPRIMIR QUE ELE FOI PARA A DIREITA.
-    while(escolha == 2){
-        printf("O cavalo se moveu para: direita.\n");
-        break;
-    }
-    movcavalo++;
-
-
-
-}
+    mover_cavalo(escolha);
        
-
-    
-    
-    
-
-
-    
-
-
-
-
-
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
 }
